5c82ebe32c8a62c705f8fa66c45a467f63ebdecf
chaitanya190030298 <73324659+chaitanya190030298@users.noreply.github.com>
2021-06-02 19:44:32 +0530

    Add files via upload

---
+++ part2/The Phonebook/excerse2.10/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import PhoneBook from './components/phonebook'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+
+
+
+const App = (props) => {
+  const [ persons, setPersons] = useState(props.persons) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [filter, setFilter] = useState('')
+
+
+const addDetails=(event)=>{
+  event.preventDefault()
+  if(persons.find(persons=>persons.name===newName)){
+    alert(newName+" is already added to the phonebook")
+  }
+  else if(persons.find(persons=>persons.number===newNumber)){
+    alert(newNumber+" is already added to the phonebook")
+  }
+  else{
+  const nameObject={
+    content:newName,
+    number:newNumber,
+    id:persons.length+1
+  }
+  setPersons(persons.concat(nameObject))
+  setNewName('')
+  setNewNumber('')
+}
+}
+const handleChange=(event)=>{
+
+  setNewName(event.target.value)
+}
+const handleChangeNumber=(event)=>{
+
+  setNewNumber(event.target.value)
+}
+const handleFilterChange = (e) => {
+  setFilter(e.target.value)
+
+}
+const display=persons.filter((filtered)=>{
+    if(filter==='')
+    {
+      return persons
+    }
+    else if(filtered.content.toLowerCase().includes(filter.toLowerCase()))  {
+        return filtered
+    }
+
+  })
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter onChange={handleFilterChange}/>
+      <PersonForm onSubmit={addDetails} valueName={newName} onChangeName={handleChange} valueNumber={newNumber} onChangeNumber={handleChangeNumber}/>
+      <h2>Numbers</h2>
+       {display.map(person=>
+        <PhoneBook key={person.id} person={person} />
+        )}
+    </div>
+  )
+
+}
+
+export default App
