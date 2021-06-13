16c98d17f3df4267b53d4f36cadc68c867d63a18
chaitanya190030298 <73324659+chaitanya190030298@users.noreply.github.com>
2021-06-13 16:43:13 +0530

    Create person.js

---
+++ part3/phonebook/excerise3.20/testnode/models/person.js
@@ -0,0 +1,35 @@
+  
+const mongoose=require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url=process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.then(result => {
+  console.log('connected to MongoDB')
+})
+.catch((error) => {
+  console.log('error connecting to MongoDB:', error.message)
+})
+
+const personSchema=new mongoose.Schema({
+  name:{ type:String, minLength:3, unique: true},
+  number:{ type:String,minLength:8 ,unique: true},
+
+})
+personSchema.plugin(uniqueValidator);
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+module.exports=mongoose.model('Person',personSchema)
